3d99586c264a2b9f1fa6e7e71925b7366b27e19f
AngelicaCorrales <angelicacorrales2002@gmail.com>
2021-11-26 20:31:59 -0500

    Bienco: relationships added

--- src/model/Bienco.java
+++ src/model/Bienco.java
@@ -1,5 +1,65 @@
 package model;
 
+import java.util.ArrayList;
+
+import dataStructures.Graph;
+import dataStructures.GraphAL;
+import dataStructures.GraphAM;
+
 public class Bienco {
 
+	private Graph<Building> graph;
+	private GraphAL<Building> graphAL;
+	private GraphAM<Building> graphAM;
+	
+	private ArrayList<Building> buildings;
+	
+	
+	public Bienco() {
+		buildings=new ArrayList<>();
+	}
+
+
+	public Graph<Building> getGraph() {
+		return graph;
+	}
+
+
+	public void setGraph(Graph<Building> graph) {
+		this.graph = graph;
+	}
+
+
+	public ArrayList<Building> getBuildings() {
+		return buildings;
+	}
+
+
+	public void setBuildings(ArrayList<Building> buildings) {
+		this.buildings = buildings;
+	}
+
+
+	public GraphAL<Building> getGraphAL() {
+		return graphAL;
+	}
+
+
+	public void setGraphAL(GraphAL<Building> graphAL) {
+		this.graphAL = graphAL;
+	}
+
+
+	public GraphAM<Building> getGraphAM() {
+		return graphAM;
+	}
+
+
+	public void setGraphAM(GraphAM<Building> graphAM) {
+		this.graphAM = graphAM;
+	}
+	
+	
+	
+	
 }
