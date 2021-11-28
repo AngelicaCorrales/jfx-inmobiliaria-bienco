78d3e73118360179d0bd40ddc1bf2fccc9174acd
KerenLopez <kerenlopezcordoba@outlook.es>
2021-11-27 21:29:03 -0500

    All the tests related to add, remove and update building work correctly. Some changes were made to them in order for this to happen.

--- test/model/BiencoTest.java
+++ test/model/BiencoTest.java
@@ -21,8 +21,9 @@ public class BiencoTest {
 		bienco.getBuildings().add(new Building("Calle 5B # 3-10", "Porvenir", Zone.SUR, TypeOfBuilding.LOCAL, 620000, false, "Local de 500 metros cuadrados con segundo piso que contiene un banio"));
 		bienco.getBuildings().add(new Building("Calle 7A # 4-15", "Vipasa", Zone.NORTE, TypeOfBuilding.CASA, 205000000, true, "Casa de 3 plantas que contiene 4 habitaciones, sala, terraza, comedor, 2 banios, garaje y patio trasero."));
 		bienco.getBuildings().add(new Building("Calle 3A # 5-29", "Porvenir", Zone.SUR, TypeOfBuilding.APARTAESTUDIO, 1000000, false, "Apartaestudio amoblado ubicado en segundo piso. Contiene 2 habitaciones, sala, cocina, 1 banio."));
-		bienco.getBuildings().add(new Building("Calle 9B # 8-19", "Vipasa", Zone.NORTE, TypeOfBuilding.CASA, 350000000, true, "Casa de 2 plantas que contiene 3 habitaciones, sala, comedor, 2 banios, garaje pequeÃ±o y un patio trasero."));
+		bienco.getBuildings().add(new Building("Calle 9B # 8-19", "Vipasa", Zone.NORTE, TypeOfBuilding.CASA, 350000000, true, "Casa de 2 plantas que contiene 3 habitaciones, sala, comedor, 2 banios, garaje pequenio y un patio trasero."));
 		bienco.getBuildings().add(new Building("Calle 11B # 2-16", "Vipasa", Zone.NORTE, TypeOfBuilding.APARTAMENTO, 275000000, false, "Apartamento de 300 metros cuadrados que tiene 2 habitaciones, banio,  comedor, sitio para lavado y un pequenio balcon."));
+		bienco.getBuildings().add(new Building("Calle 58A # 1-15", "Santa Teresita", Zone.OESTE, TypeOfBuilding.APARTAMENTO, 400000000, true, "Apartamento de 400 metros cuadrados que tiene 4 habitaciones, 2 banios,  sala, y balcon."));
 		bienco.getBuildings().add(new Building("Calle 14C # 6-27", "El Prado", Zone.ESTE, TypeOfBuilding.CASA, 450000000, true, "Casa de 350 metros cuadrados que tiene 4 habitaciones y espacio para estudio, 2 banios,  sala, patio y balcon."));
 		bienco.getBuildings().add(new Building("Calle 16C # 9-27", "Pance", Zone.SUR, TypeOfBuilding.FINCA, 800000000, true, "Finca  de 1000 metros cuadrados que tiene 6 habitaciones, 3 banios,  sala, zona verde, piscina."));
 		bienco.getBuildings().add(new Building("Calle 77C # 7-43", "Los Cristales", Zone.OESTE, TypeOfBuilding.APARTAMENTO, 360000000, true, "Apartamento de 250 metros cuadrados que cuenta con 3 habitaciones, 2 banios,  sala. Ubicado en primer piso."));
@@ -68,7 +69,7 @@ public class BiencoTest {
 			boolean added = bienco.addBuilding(address, neighborhood, zone, type, price, forSale, observations);
 			assertTrue(added);
 			assertEquals(bienco.getBuildings().size(), 11);
-			assertEquals(bienco.getBuildings().get(0).getAddress(), "Calle 15 # 4-89");
+			assertEquals(bienco.getBuildings().get(10).getAddress(), "Calle 15 # 4-89");
 		} catch (NoValueException e) {
 			fail("NoValueException not expected");
 		} catch (NegativeValueException e) {
@@ -86,13 +87,11 @@ public class BiencoTest {
 		String price = "-800000";
 		boolean forSale = false;
 		String observations = "Apartaestudio ubicado en el 5 piso de la torre A del conjunto residencial Verano, cuenta con 1 habitación, baño, balcon y sala de estar.";
-		boolean added = true;
 		try {
-			added = bienco.addBuilding(address, neighborhood, zone, type, price, forSale, observations);
+			bienco.addBuilding(address, neighborhood, zone, type, price, forSale, observations);
 		} catch (NoValueException e) {
 			fail("NoValueException not expected");
 		} catch (NegativeValueException e) {
-			assertFalse(added);
 			assertEquals(bienco.getBuildings().size(), 10);
 		}
 	}
@@ -107,11 +106,9 @@ public class BiencoTest {
 		String price = "0";
 		boolean forSale = false;
 		String observations = "Apartaestudio ubicado en el 5 piso de la torre A del conjunto residencial Verano, cuenta con 1 habitación, baño, balcon y sala de estar.";
-		boolean added = true;
 		try {
-			added = bienco.addBuilding(address, neighborhood, zone, type, price, forSale, observations);
+			bienco.addBuilding(address, neighborhood, zone, type, price, forSale, observations);
 		} catch (NoValueException e) {
-			assertFalse(added);
 			assertEquals(bienco.getBuildings().size(), 10);
 		} catch (NegativeValueException e) {
 			fail("NegativeValueException not expected");
@@ -157,7 +154,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding1() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
@@ -170,9 +167,9 @@ public class BiencoTest {
 			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), 550000.0);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==550000.0);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -184,26 +181,24 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding2() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
 		String newType = "Casa";
 		String newPrice = "-500000";
 		boolean forSale = false;
-		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
-		boolean updated = true;
+		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 banio y un pequenio patio trasero.";
 		try {
-			updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
+			bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
 		} catch (NoValueException e) {
 			fail("NoValueException not expected");
 		} catch (NegativeValueException e) {
-			assertFalse(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), 580000.0);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000.0);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		}
@@ -211,24 +206,22 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding3() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
 		String newType = "Casa";
 		String newPrice = "0";
 		boolean forSale = false;
-		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
-		boolean updated = true;
+		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 banio y un pequenio patio trasero.";
 		try {
-			updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
+			bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
 		} catch (NoValueException e) {
-			assertFalse(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), 580000.0);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000.0);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NegativeValueException e) {
@@ -238,22 +231,22 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding4() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 5B # 3-10";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
 		String newType = "Casa";
 		String newPrice = "580000";
 		boolean forSale = false;
-		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
+		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 banio y un pequenio patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
 			assertFalse(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), "Calle 4C # 5-29");
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -265,7 +258,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding5() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 3-19";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
@@ -275,12 +268,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -292,7 +285,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding6() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Popular";
 		String newZone = "Sur";
@@ -302,12 +295,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -319,7 +312,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding7() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Oeste";
@@ -329,12 +322,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.OESTE);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.OESTE.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -346,7 +339,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding8() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
@@ -356,12 +349,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.APARTAMENTO.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -373,7 +366,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding9() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
@@ -383,12 +376,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta que contiene 2 habitaciones, sala, comedor, 1 baño y un pequeño patio trasero.";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertTrue(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
@@ -400,7 +393,7 @@ public class BiencoTest {
 	
 	@Test
 	public void testUpdateBuilding10() {
-		setupScenary2();
+		setupScenary1();
 		String newAddress = "Calle 4C # 5-29";
 		String newNeighborhood = "Porvenir";
 		String newZone = "Sur";
@@ -410,12 +403,12 @@ public class BiencoTest {
 		String newObservations = "Casa de 1 planta";
 		try {
 			boolean updated = bienco.updateBuilding(bienco.getBuildings().get(0), newAddress, newNeighborhood, newZone, newType, newPrice, forSale, newObservations);
-			assertFalse(updated);
+			assertTrue(updated);
 			assertEquals(bienco.getBuildings().get(0).getAddress(), newAddress);
 			assertEquals(bienco.getBuildings().get(0).getNeighborhood(), newNeighborhood);
-			assertEquals(bienco.getBuildings().get(0).getZone(), Zone.SUR);
-			assertEquals(bienco.getBuildings().get(0).getType(), TypeOfBuilding.CASA);
-			assertEquals(bienco.getBuildings().get(0).getPrice(), newPrice);
+			assertEquals(bienco.getBuildings().get(0).getZone().toString(), Zone.SUR.toString());
+			assertEquals(bienco.getBuildings().get(0).getType().toString(), TypeOfBuilding.CASA.toString());
+			assertTrue(bienco.getBuildings().get(0).getPrice()==580000);
 			assertFalse(bienco.getBuildings().get(0).isForSale());
 			assertEquals(bienco.getBuildings().get(0).getObservations(), newObservations);
 		} catch (NoValueException e) {
